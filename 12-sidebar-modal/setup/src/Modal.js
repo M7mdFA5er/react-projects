653b27f99dc35bfabeff9eb292544a5d626dc784
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-09-09 10:17:44 +0200

    Project 12 Finish.

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useGlobalConxtet } from './context';
+
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalConxtet();
   return (
-    <div className={`modal-overlay`}>
+    <div className={`modal-overlay ${isModalOpen && 'show-modal'}`}>
       <div className="modal-container">
         <h3>modal contet</h3>
-        <button className="close-modal-btn">
+        <button className="close-modal-btn" onClick={closeModal}>
           <FaTimes />
         </button>
       </div>
