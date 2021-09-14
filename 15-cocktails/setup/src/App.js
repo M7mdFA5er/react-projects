1765ba8b92263a157d52ee52ffccd5df68fd6193
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-09-14 21:08:14 +0200

    Project 15 - Navbar.

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -10,8 +10,8 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <>
-      <Navbar />
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About}></Route>
