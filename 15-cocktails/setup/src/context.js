97cbb9d34206e74184a9f146ca634c6391e5ae3c
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-09-15 22:03:26 +0200

    Project 15 - Single Cocktail Component.

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -10,7 +10,7 @@ const AppProvider = ({ children }) => {
   const [cocktails, setCocktails] = useState([]);
 
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
@@ -37,11 +37,11 @@ const AppProvider = ({ children }) => {
       console.log('error :>> ', error);
       setLoading(false);
     }
-  }
+  }, [searchTerm])
 
   useEffect(() => {
     fetchDrinks();
-  }, [searchTerm])
+  }, [searchTerm, fetchDrinks])
 
   return (
     <AppContext.Provider
