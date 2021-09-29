5f43bda773e2a497d0f0cc56e4a2aea8cddbc9f1
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-09-29 23:18:09 +0200

    Project_21 Finish

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -1,37 +1,15 @@
 import React, { useState, useContext, useEffect } from 'react'
+import useFetch from './useFetch'
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [error, setError] = useState({ show: false, msg: '' });
-  const [isLoading, setIsLoading] = useState(true);
+
   const [query, setQuery] = useState('batman');
-  const [movies, setMovies] = useState([]);
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`);
 
-  const fetchMovies = async (url) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      if (data.Response === 'True') {
-        setMovies(data.Search);
-        setError({ show: false, msg: '' });
-      }
-      else {
-        setError({ show: true, msg: data.Error })
-      }
-      setIsLoading(false);
-      console.log('data :>> ', data);
-    } catch (error) {
-      console.log('error :>> ', error);
-    }
-  }
-
-
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`)
-  }, [query])
 
 
   return (
---
+++ 21-movie-db/setup/src/useFetch.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
+function useFetch(urlParams) {
+
+  const [error, setError] = useState({ show: false, msg: '' });
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  const fetchData = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.Response === 'True') {
+        setData(data.Search || data);
+        setError({ show: false, msg: '' });
+      }
+      else {
+        setError({ show: true, msg: data.Error })
+      }
+      setIsLoading(false);
+      console.log('data :>> ', data);
+    } catch (error) {
+      console.log('error :>> ', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(`${API_ENDPOINT}&s=${urlParams}`)
+  }, [urlParams])
+
+  return { isLoading, error, data }
+}
+
+export default useFetch
