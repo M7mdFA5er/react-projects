241e1e5e8cf6b45f2af2b50ffec19247854b7c59
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-10-02 22:32:47 +0200

    Project_23 Finish

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -5,14 +5,21 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  const { waiting, loading, questions, index, correct } = useGlobalContext();
+  const { waiting, loading, questions, index, correct, nextQuestion, checkAnswer } = useGlobalContext();
   if (waiting) return <SetupForm />
   if (loading) return <Loading />
   const { question, incorrect_answers, correct_answer } = questions[index];
-  const answers = [...incorrect_answers, correct_answer];
+  //const answers = [...incorrect_answers, correct_answer];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) answers.push(correct_answer);
+  else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
   return (
     <main>
-      {/* <Modal /> */}
+      <Modal />
       <section className='quiz'>
         <p className="correct-answers">correct answers :{correct}/{index}</p>
         <article className='container'>
@@ -20,12 +27,19 @@ function App() {
           <div className="btn-container">
             {
               answers.map((answer, index) => {
-                return <button key={index} className='answer-btn' dangerouslySetInnerHTML={{ __html: answer }} />
+                return (
+                  <button
+                    key={index}
+                    className='answer-btn'
+                    dangerouslySetInnerHTML={{ __html: answer }}
+                    onClick={() => checkAnswer(correct_answer === answer)}
+                  />
+                )
               })
             }
           </div>
         </article>
-        <button className='next-question'>next Question</button>
+        <button className='next-question' onClick={nextQuestion}>next Question</button>
       </section>
     </main>
   )
--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -21,7 +21,7 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
-
+  const [quiz, setQuiz] = useState({ amount: 10, category: 'sports', difficulty: 'easy' });
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const fetchQuestions = async (url) => {
@@ -46,9 +46,48 @@ const AppProvider = ({ children }) => {
     }
   }
 
-  useEffect(() => {
-    fetchQuestions(tempUrl)
-  }, []);
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1;
+      if (index > questions.length - 1) {
+        openModal()
+        return 0;
+      } else {
+        return index;
+      }
+    });
+  }
+
+  const checkAnswer = value => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1);
+    }
+    nextQuestion();
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true);
+
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setWaiting(true);
+    setCorrect(0);
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuiz({ ...quiz, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { amount, category, difficulty } = quiz
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+    fetchQuestions(url);
+  }
 
   return (
     <AppContext.Provider
@@ -59,7 +98,13 @@ const AppProvider = ({ children }) => {
         index,
         correct,
         error,
-        isModalOpen
+        isModalOpen,
+        nextQuestion,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit
       }}>
       {children}
     </AppContext.Provider>)
